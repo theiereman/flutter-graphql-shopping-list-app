2dfcef253954fa856ad065470c3a9ea822b2b8da
Thomas Eyermann <thomas.eyermann@protonmail.com>
2024-02-11 18:07:26 +0100

    refactor de l'arbo avec projet de base fonctionnels

--- backend/src/resolvers/mutations.ts
+++
@@ -1,9 +0,0 @@
-import { MutationResolvers } from "../__generated__/resolvers-types";
-
-const mutations: MutationResolvers = {
-  addUser: async (_, { email, password }, { dataSources }) => {
-    return dataSources.usersAPI.addUser({ email, password });
-  },
-};
-
-export default mutations;
--- backend/src/resolvers/queries.ts
+++
@@ -1,9 +0,0 @@
-import { QueryResolvers } from "../__generated__/resolvers-types";
-
-const queries: QueryResolvers = {
-  users: async (_, __, { dataSources }) => {
-    return dataSources.usersAPI.getUsers();
-  },
-};
-
-export default queries;
