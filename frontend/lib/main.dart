30f5c999a7e9b31df0266194049c913c1e79779a
Thomas Eyermann <thomas.eyermann@protonmail.com>
2024-07-22 19:16:17 +0200

    added provider observer for debug

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend/src/utils/custom_provider_observer.dart';
 
 import 'src/app.dart';
 
 void main() {
-  runApp(const ProviderScope(child: App()));
+  runApp(
+      ProviderScope(observers: [CustomProviderObserver()], child: const App()));
 }
