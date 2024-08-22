af0abf6a08802bc2f9749454815e158cf240adea
Thomas Eyermann <thomas.eyermann@protonmail.com>
2024-08-22 20:20:48 +0200

    feat(items): added repository to add new items to a list

---
+++ frontend/lib/src/features/items/data/repository/item_repository.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'item_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$itemRepositoryHash() => r'7eedd153b2328ae501063139220030ea12e7af2f';
+
+/// See also [itemRepository].
+@ProviderFor(itemRepository)
+final itemRepositoryProvider = Provider<ItemRepository>.internal(
+  itemRepository,
+  name: r'itemRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$itemRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ItemRepositoryRef = ProviderRef<ItemRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
